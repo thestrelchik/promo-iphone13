398dd5f1f826022f92991c354ece21339f888cdc
Andrei Strelchik <thestrelchik@gmail.com>
2022-01-21 22:53:16 +0300

    promo-iphone13
    promo-iphone13

---
+++ script/scroll.js
@@ -0,0 +1,22 @@
+const links = document.querySelectorAll('.header-menu__item a')
+
+
+links.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        event.preventDefault()
+        
+        const id = element.getAttribute('href').substring(1)
+        const section = document.getElementById(id)
+        
+        if (section) {
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            })
+        } else {
+            console.log('нет');
+        }
+    })
+    
+})
+
