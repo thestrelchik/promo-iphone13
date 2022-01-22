fa1c5ea19d7a8fcf0818fa9e719d515e88cc8c26
Andrei Strelchik <thestrelchik@gmail.com>
2022-01-22 20:46:25 +0300

    работа над tabs

--- script/scroll.js
+++ script/scroll.js
@@ -1,22 +1,31 @@
-const links = document.querySelectorAll('.header-menu__item a')
+const scrollFunc = () => {
+
+    const links = document.querySelectorAll('.header-menu__item a')
+    const linkCharacteristics = document.querySelector('.card-details__link-characteristics')
+    const newArray = [...links, linkCharacteristics]
 
 
-links.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        event.preventDefault()
-        
-        const id = element.getAttribute('href').substring(1)
-        const section = document.getElementById(id)
-        
-        if (section) {
-            section.scrollIntoView({
+
+    newArray.forEach((element) => {
+        element.addEventListener('click', (event) => {
+            event.preventDefault()
+            
+            const id = element.getAttribute('href').substring(1)
+            const section = document.getElementById(id)
+            
+            if (section) {
+                    section.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                })
+            } else {
+                section.scrollIntoView(document.querySelector("#characteristics"), { 
                 behavior: 'smooth',
                 block: 'start'
-            })
-        } else {
-            console.log('нет');
-        }
+                 });
+            }
+        })  
     })
-    
-})
+}
 
+scrollFunc()
